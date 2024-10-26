67111e0b7b13d1efcc3e9b80ac447a5e161f775e
BH364 <balagaharika9@gmail.com>
2024-10-26 18:16:03 +0530

    exploring

--- src/app.js
+++ src/app.js
@@ -1,17 +1,54 @@
 const express=require('express');
 
 const app=express();
-app.use("/test",(req,res)=>{
-    res.send("This is test page");
+
+// app.use("/test",(req,res)=>{
+//     res.send("This is test page");
+// })
+
+app.get("/test",(req,res)=>{
+    res.send("Get the data");
 })
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello world");
-})
-app.use("/",(req,res)=>{
-    res.send("This is message from backend");
+app.post('/test',(req,res)=>{
+    res.send("Post the data");
 })
 
+app.delete('/test',(req,res)=>{
+    res.send("Deleted successfully");
+})
+
+app.patch("/test",(req,res)=>{
+    res.send("Path the data");
+})
+
+app.put("/test",(req,res)=>{
+    res.send("put the data");
+})
+
+app.get("/user",(req,res)=>{
+    console.log(req.query);
+    res.send("sucessfull");
+})
+
+// app.get("/user/:userId/:name",(req,res)=>{
+//     console.log(req.params);
+//     res.send("put the data");
+// })
+// app.get(/.*fly$/,(req,res)=>{
+//     res.send("Put the data");
+// })
+
+// app.get('/ab(de)+c',(req,res)=>{
+//     res.send("Executed");
+// })
+// app.get('/ab+c',(req,res)=>{
+//     res.send("Executed");
+// })
+
+// app.get('/ab?c',(req,res)=>{
+//     res.send("Executed");
+// })
 
 app.listen(7777,()=>{
     console.log("server running successfully");
